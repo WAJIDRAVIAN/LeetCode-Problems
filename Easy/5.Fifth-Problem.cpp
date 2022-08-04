840378ad8f71cf19b966f0d54354dafd27fef18d
WAJIDRAVIAN <wajidh1997@gmail.com>
2022-08-04 15:33:45 +0500

    5.Fifth-Problem

--- Easy/5.Fifth-Problem.cpp
+++ Easy/5.Fifth-Problem.cpp
@@ -2,7 +2,6 @@
 Given a string s, return the longest palindromic substring in s.
 
 
-
 Example 1:
 
 Input: s = "babad"
@@ -12,7 +11,6 @@ Example 2:
 
 Input: s = "cbbd"
 Output: "bb"
-
 
 Constraints:
 
@@ -26,49 +24,80 @@ using namespace std;
 
 class Solution
 {
+    // calculate length of given string
+    int length = 0;
+    int leftPivot = 0;
+    int rightPivot = 0;
+    int maxLength = 0;
+    string temp = "";
+    string result = "";
+
 public:
     string longestPalindrome(string s)
     {
-        // get length of input string
-        int n = s.size();
-        string result = "";
-        // All substrings of length 1
-        // are palindromes
-        int maxLength = 1, start = 0;
-
-        // Nested loop to mark start and end index
-        for (int i = 0; i < s.length(); i++)
+        if(s.size() == 1 || s.size() == 0)
+            return s;
+        length = s.size();
+        for (int i = 0; i < length / 2; i++)
         {
-            for (int j = i; j < s.length(); j++)
+            leftPivot = length / 2 - i - 1;
+            rightPivot = leftPivot + 1;
+            // for even length string
+            check_palindrom(s);
+
+            // for even length string
+            rightPivot = length / 2 + i + 1;
+            leftPivot = rightPivot - 1;
+            check_palindrom(s);
+
+            // for odd length string
+            leftPivot = length / 2 - i;
+            rightPivot = leftPivot;
+            check_palindrom(s);
+
+            // for odd length string
+            rightPivot = length / 2 + i;
+            leftPivot = rightPivot;
+            check_palindrom(s);
+        }
+
+        return result;
+    }
+
+    void check_palindrom(string s)
+    {
+        for (int j = 0; j < length / 2 + 1 && leftPivot >= 0 && rightPivot < length; j++)
+        {
+            //cout << "leftPivot: " << leftPivot << " rightPivot: " << rightPivot << endl;
+
+            if (s[leftPivot] == s[rightPivot])
             {
-                int flag = 1;
-
-                // Check palindrome
-                for (int k = 0; k < (j - i + 1) / 2; k++)
-                    if (s[i + k] != s[j - k])
-                    {
-                        flag = 0;
-                    }
-
-                // Palindrome
-                if (flag && (j - i + 1) > maxLength)
-                {
-                    start = i;
-                    maxLength = j - i + 1;
-                }
+                leftPivot--;
+                rightPivot++;
+            }
+            else
+            {
+                break;
             }
         }
-        for (int i = start; i <= start + maxLength - 1; ++i)
-            result = result + s[i];
-        return result;
+        // temp = s.substr(leftPivot, rightPivot - leftPivot);
+        temp = "";
+        for (int i = leftPivot + 1; i < rightPivot; i++)
+        {
+            temp += s[i];
+        }
+        if (result.size() < temp.size())
+        {
+            result = temp;
+            
+        }
     }
 };
 
 int main()
 {
-
     Solution s;
-    string str = "abaabaab";
+    string str = "a"; //"gphyvqruxjmwhonjjrgumxjhfyupajxbjgthzdvrdqmdouuukeaxhjumkmmhdglqrrohydrmbvtuwstgkobyzjjtdtjroqpyusfsbjlusekghtfbdctvgmqzeybnwzlhdnhwzptgkzmujfldoiejmvxnorvbiubfflygrkedyirienybosqzrkbpcfidvkkafftgzwrcitqizelhfsruwmtrgaocjcyxdkovtdennrkmxwpdsxpxuarhgusizmwakrmhdwcgvfljhzcskclgrvvbrkesojyhofwqiwhiupujmkcvlywjtmbncurxxmpdskupyvvweuhbsnanzfioirecfxvmgcpwrpmbhmkdtckhvbxnsbcifhqwjjczfokovpqyjmbywtpaqcfjowxnmtirdsfeujyogbzjnjcmqyzciwjqxxgrxblvqbutqittroqadqlsdzihngpfpjovbkpeveidjpfjktavvwurqrgqdomiibfgqxwybcyovysydxyyymmiuwovnevzsjisdwgkcbsookbarezbhnwyqthcvzyodbcwjptvigcphawzxouixhbpezzirbhvomqhxkfdbokblqmrhhioyqubpyqhjrnwhjxsrodtblqxkhezubprqftrqcyrzwywqrgockioqdmzuqjkpmsyohtlcnesbgzqhkalwixfcgyeqdzhnnlzawrdgskurcxfbekbspupbduxqxjeczpmdvssikbivjhinaopbabrmvscthvoqqbkgekcgyrelxkwoawpbrcbszelnxlyikbulgmlwyffurimlfxurjsbzgddxbgqpcdsuutfiivjbyqzhprdqhahpgenjkbiukurvdwapuewrbehczrtswubthodv";
     cout << s.longestPalindrome(str) << endl;
     return 0;
 }
