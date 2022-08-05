578058dcca7c10aa208abc57bd3be2fa41c19b34
WAJIDRAVIAN <wajidh1997@gmail.com>
2022-08-05 13:56:17 +0500

    9.Ninth-Problem

---
+++ Easy/9.Ninth-Problem.cpp
@@ -0,0 +1,63 @@
+/*
+Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward.
+
+For example, 121 is a palindrome while 123 is not.
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+ 
+
+Follow up: Could you solve it without converting the integer to a string?
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0)
+            return false;
+        if(x<10)
+            return true;
+        string s = to_string(x);
+        int size = s.length();
+        for (int i = 0; i < size; i++)
+        {
+            if(s[i] != s[size-1-i])
+                return false;
+        }
+        return true;
+    }
+};
+
+int main()
+{
+    Solution s;
+    cout << s.isPalindrome(121) << endl;
+    cout << s.isPalindrome(-121) << endl;
+    cout << s.isPalindrome(10) << endl;
+    return 0;
+}
