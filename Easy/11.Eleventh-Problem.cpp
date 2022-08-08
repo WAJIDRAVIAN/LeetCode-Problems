32eba3416228b9bcfece97df46bd23efa020c4ed
WAJIDRAVIAN <wajidh1997@gmail.com>
2022-08-08 19:26:31 +0500

    11.Eleventh_Problem

--- Easy/11.Eleventh-Problem.cpp
+++ Easy/11.Eleventh-Problem.cpp
@@ -7,7 +7,7 @@ Return the maximum amount of water a container can store.
 
 Notice that you may not slant the container.
 
- 
+
 
 Example 1:
 
@@ -19,7 +19,7 @@ Example 2:
 
 Input: height = [1,1]
 Output: 1
- 
+
 
 Constraints:
 
@@ -32,15 +32,77 @@ n == height.length
 #include <vector>
 using namespace std;
 
+class Solution
+{
+public:
+    int maxArea(vector<int> &height)
+    {
+        int size = height.size();
+        if(size==2){
+            if(height[0]>height[1])
+            return height[1];
+            else
+            return height[0];
+        }
+
+        int max1 = 0;
+        
+        int area = -1;
+        int startIndex = 0;
+        int endIndex = size -1;
+        for (int i = 0; i < size; i++)
+        {
+            // calculate last row
+            if (height[startIndex] < height[endIndex])
+            {
+                max1 = height[startIndex] * (endIndex - startIndex);
+                startIndex++;
+            }
+            else if(startIndex != endIndex)
+            {
+                max1 = height[endIndex] * (endIndex - startIndex);
+                endIndex--;
+            }
+
+            if (max1 > area)
+            {
+                area = max1;
+            }
+        }
+
+        return area;
+    }
+};
+// Driver function definition
+int main()
+{
+    vector<int> height = {2,1};
+    Solution s;
+    cout<< s.maxArea(height)<<endl;
+    return 0;
+}
+
+/*
 
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        
+        int max = 0;
+        int area = 0;
+        for (int i = 0; i < height.size(); i++) {
+            for(int j = i+1; j < height.size(); j++) {
+                if(height[i]< height[j]) {
+                    area= height[i]* (j-i);
+                }
+                else{
+                    area= height[j]* (j-i);
+                }
+                if(area>max){
+                    max = area;
+                }
+            }
+        }
+        return max;
     }
 };
-//Driver function definition
-int main(){
-
-    return 0;
-}
+*/
